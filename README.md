f9dbef0ca1a32ba20458950a2f03c5dad01bd009
Adam Buecher <abuecher@gmail.com>
2024-03-16 16:22:28 -0700

    Added basic sketch of the class model.
    Added overview of classes to use and approach. Set to C++11 since that's
    what Joby uses.

--- README.md
+++ README.md
@@ -8,13 +8,10 @@
 - TODO and NOTE are OK for things that might take too long to properly implement.
 -
 
-
-
 # Concepts
 
 The table provided can help determine what components I need to model:
 
-(Can remove these ticks in emacs, but the CLion WYSIWYG editor does not render well)
 ```
 |----------------------|-------|-------|---------|-------|------|------------------------------------------------|
 | Company Name         | Alpha | Bravo | Charlie | Delta | Echo | Notes                                          |
@@ -37,9 +34,46 @@ The table provided can help determine what components I need to model:
 |                      |       |       |         |       |      |                                                |
 |----------------------|-------|-------|---------|-------|------|------------------------------------------------|
 ```
+# Design Ideas
+
+Decomposing what is needed based on above data table:
+Things on or about Aircraft:
+- Aircraft - wraps all other components. Instances of this are what are measured.
+- Battery - stats: charging_rate, total_charge
+- Engine - cruise_speed, battery_drain,
+- Cabin - Where the passengers are. Limited utility. Should I just make this a single int in the Aircraft class
 
 
+External to the aircraft
+- Charger - Limited resource. Have a Mutex or somesuch.
+- Clock. Do I have an actual ticking clock or do I have some centralized event queue set current time to the next event trigger time (track the time based on that).
+  - Seems pretty obvious once modelikng the table data as a struct-per-model that I will just simulate clock with events, No need to build a ticking clock.
 
-# Note
-I wrote most of this in emacs (WSL emacs-GUI). It's nice for the mouse-free navigation (Markjdown-major mode much like
-orgmode for editing ascii tables even if they dont render properly w/o the tick marks.
+
+# Deliverables
+From the word doc:
+
+You will simulate using these vehicle for 3 hours. Of course your simulation should take much less time than that. 20 total vehicles should be deployed, and a random number of each type of vehicle should be used (with the total between all five types being 20).
+
+There are only three chargers available for all 20 vehicles! A single charger can only be used by one vehicle at a time. Assume the chargers can charge the batteries in the Time to Charge time listed for each vehicle.
+
+Keep track of the following statistics per vehicle type:
+- average flight time per flight
+- average distance traveled per flight
+- average time charging per charge session
+- total number of faults
+- total number of passenger miles.
+  For example, if there are 2 vehicles carrying 4 passengers on a vehicle that cruises    for 1 hour at 100 mph, total number of passenger miles is 2 * 4 * 1 * 100 = 800.
+
+Assume that:
+- Each vehicle starts the simulation with a fully-charged battery
+- Each vehicle instantaneously reaches Cruise Speed
+- Each vehicle is airborne for the full use of the battery, and is immediately in line for the charger after running out of battery power.
+
+Please include the statistics recorded during at least one run of the simulation in your submission.
+
+Please do not hesitate to reach out to ask any questions about the problem! However, if you find yourself making an assumption that we haven’t explained explicitly, you may simply document your assumption. It’s interesting to us to hear what assumptions you made and why.
+
+## questions
+- faults. Is there any downtime associated with those? I will assume not and that I should just count them, but need to verify that is correct.
+
--- main.cpp
+++ main.cpp
@@ -1,12 +1,99 @@
 #include <iostream>
+#include <vector>
 
-
+// ADAM_DEBUG sketching out basic ideas.
 /*
  * Ideas for building a simulation of a simplified electric vehicle. The simulation will simulate the distance traveled,
  * energy consumed stops, refueling time, etc. I will model the vehicle using OOP and break it down in the following
- * manner
+ * manner.
+ *
+ * Possible ways to model this:
+ * 1. AircraftModel is composed of classes for Engine, Battery, Cabin. This is more like I'd expect it to be in a more
+ *     complex, real world scenario since you'd need to be able to simulate swapping out engines, batteries, etc.
+ * 2. One AircraftModel class containing all og he fields from the table. Each model would be an instance of that. Can
+ *     have each unique aircraft be a copy of the model object (perhaps with extra id) or have some vector counter.
+ *
+ * thoughts: going with approach 23 since (1) over-complicates what we are actually simulating by over encapsulating data.
+ * We do not need to swap any components so (1) has no upside.
+ *
+ * Need to track each aircraft separately since they will all need to deal with resource contention separately which will
+ * result in different instances of same model having separate charging times. I could have the data in the table per model
+ * in one table and each aircraft will track its own totals + modelId + aircraftId.
+ * TODO write up the above. Add some diagrams where needed.
  */
 
+typedef uint32_t Stamp_t;
+
+/**
+ * Modelling each aircraft as an object with its own state machine. May or may not break it down
+ * into components (breaking it down provides a more OOD that might be more like how one would
+ * model a more complex real-world design. On the other hand it actually complicates the simulation
+ * without much benefit since the problem does not ask one to do things like swap out batteries or
+ * repair components).
+ */
+enum States {
+    STATE_INIT,
+    STATE_FLYING,
+    // STATE_WAITING_FOR_CHARGER, // I can omit this and include the wait as charging time id the object is actively waiting for a slot.
+    STATE_CHARGING,
+};
+
+
+/**
+ * Model the provided table for models of aircraft. Since the simulation is basic, we can reuse this
+ * for the aircraft instances not just models. Either copy base model stats or something. Once
+ * airborne each aircraft will separately need to deal with resource contention for chargers even
+ * if they started at same time.
+ */
+struct AirCraftModel {
+    int passenger_count;
+    int cruise_speed; // MPH
+    int battery_life; // kWh
+    float time_to_charge; // hrs - TODO assumption: This includes time spent waiting for a charger. Find out if assumption valid.
+    float cruise_power_draw; // kWh / mile
+    float faults_per_hour; // probability if faults per hour in decimal format.
+    std::string typeName; // company name
+};
+
+enum ModelId_t {
+    ALPHA,
+    BRAVO,
+    CHARLIE,
+    DELTA,
+    ECHO,
+};
+
+/**
+ * Things each aircraft instance needs to track.
+ */
+struct Telemetry {
+    long total_flight_hours;
+    float total_miles_traveled;
+    float total_charging_time; // includes time spent waiting for a charger. TODO find out if that is a valid assumption. If not I need adjust the calculation
+    int total_faults;
+    // int num_passengers;// passengers are always max so no need to track that per instance.
+};
+
+/**
+ * This is a class rather than a struct  since I plan on using for more than passing raw data.
+ */
+struct AircraftInstance {
+    ModelId_t modelId;
+    Telemetry telemetry;
+};
+
+
+class Simulation {
+private:
+    std::vector<AircraftInstance> _aircrafts;
+
+public:
+    void create_aircrafts(); ///< populate _aircrafts with 20 random aircraft.
+    void run_simulation();
+    void generate_report() const; // generate report for all aircraft
+};
+
+// ~ADAM_DEBUG - end of rough OOD sketch. I did iterate a little on it at first.
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
